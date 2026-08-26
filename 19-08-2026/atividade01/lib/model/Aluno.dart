ee4a481e4844c4d91f1946d0b0ec6a3722592e86
jm-uscs <joao.martin200@uscsonline.com.br>
2026-08-26 20:54:14 -0300

    Organizacao das pastas

---
+++ 19-08-2026/atividade01/lib/model/Aluno.dart
@@ -0,0 +1,15 @@
+class Aluno {
+  final String nome;
+  final String email;
+  final int idade;
+  final String curso;
+  final bool receberNovidades;
+
+  Aluno({
+    required this.nome,
+    required this.email,
+    required this.idade,
+    required this.curso,
+    required this.receberNovidades,
+  });
+}
