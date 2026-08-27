c35f493672393f42bf889a902cebfeea60432294
jm-uscs <joao.martin200@uscsonline.com.br>
2026-08-26 22:13:11 -0300

    Atividade 04 elementos inciais criados

---
+++ 26-08-2026/atividade04/lib/screens/telaCirculo.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Telacirculo extends StatelessWidget{
+  const Telacirculo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tela Circulo'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsetsGeometry.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Tela Circulo'),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
